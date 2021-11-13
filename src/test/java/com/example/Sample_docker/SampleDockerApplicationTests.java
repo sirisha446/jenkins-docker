4cd165a193019a1b79d71cf923fe261d2840293e
siriiii <sirisirisha445@gmail.com>
2021-11-13 18:45:12 +0530

    firstcommit

---
+++ src/test/java/com/example/Sample_docker/SampleDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sample_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
